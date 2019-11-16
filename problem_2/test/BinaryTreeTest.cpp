400a6c8ce9fdd645fe266df963d525c0cc9c80ad
Luo-Leng <lengluo97@gmail.com>
2019-11-16 15:51:18 -0800

    LCA UnitTest Done

--- problem_2/test/BinaryTreeTest.cpp
+++ problem_2/test/BinaryTreeTest.cpp
@@ -1,6 +1,23 @@
 #include "BinaryTree.h"
 #include "gtest/gtest.h"
 
+
+
+BinaryTree<int>* generateTree() {
+    TreeNode<int>* root = new TreeNode<int>(4);
+    root->setLeft(new TreeNode<int>(8));
+    root->setRight(new TreeNode<int>(6));
+
+    root->getLeft()->setLeft(new TreeNode<int>(7));
+    root->getLeft()->setRight(new TreeNode<int>(3));
+
+    root->getRight()->setLeft(new TreeNode<int>(2));
+    root->getRight()->setRight(new TreeNode<int>(9));
+    BinaryTree<int> *tree = new BinaryTree<int>(root);
+
+    return tree;
+}
+
 TEST(tree, demo) {
     TreeNode<int>* root = new TreeNode<int>(3);
     root->setLeft(new TreeNode<int>(1));
@@ -14,5 +31,15 @@ TEST(tree, demo) {
     BinaryTree<int> tree(root);
 
     ASSERT_EQ(tree.height(),3);
+}
+TEST(tree, LCA) {
+    BinaryTree<int>* tree = generateTree();
+    ASSERT_EQ(tree->LCA(4, 4), 4);
+    ASSERT_EQ(tree->LCA(7, 7), 7);
+    ASSERT_EQ(tree->LCA(7, 3), 8);
+    ASSERT_EQ(tree->LCA(7, 8), 8);
+    ASSERT_EQ(tree->LCA(8, 6), 4);
+    ASSERT_EQ(tree->LCA(3, 2), 4);
+delete tree;
 }
 
